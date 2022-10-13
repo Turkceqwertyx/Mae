64bc17ab6a8d3ccb7eb001870a61cb679c978ff2
turkc <turkc@DESKTOP-L4BJ6I2>
2022-10-13 17:54:03 +0300

    Proje dosyası ekle.

---
+++ Project1/Kaynak1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int pin = 2389;
+	int trie = 0;
+
+	cout << "Lütfen parolayý girin: \n";
+	cin >> pin;
+
+	trie++;
+
+	while(pin != 2389)
+	return 0;
+}
